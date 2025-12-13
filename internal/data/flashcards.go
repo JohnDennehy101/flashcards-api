450867ea7020a93bf3624a8c348a4f3c3a6969d8
John Dennehy <johndennehy101@gmail.com>
2025-12-13 17:15:00 +0000

    Add delete flashcard db functionality

--- internal/data/flashcards.go
+++ internal/data/flashcards.go
@@ -212,5 +212,27 @@ func (m FlashcardModel) Update(flashcard *Flashcard) error {
 }
 
 func (m FlashcardModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `
+        DELETE FROM flashcards
+        WHERE id = $1`
+
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
 	return nil
 }
