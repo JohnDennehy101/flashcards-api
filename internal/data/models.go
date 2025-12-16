a1f78c27790a2d5340e27e2413f4957919b7285b
John Dennehy <johndennehy101@gmail.com>
2025-12-16 22:16:14 +0000

    Add user account activation functionality

--- internal/data/models.go
+++ internal/data/models.go
@@ -13,11 +13,13 @@ var (
 type Models struct {
 	Flashcards FlashcardModel
 	Users      UserModel
+	Tokens     TokenModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Flashcards: FlashcardModel{DB: db},
+		Tokens:     TokenModel{DB: db},
 		Users:      UserModel{DB: db},
 	}
 }
